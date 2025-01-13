28169b150aa48b2be879c236ab5d2bbcf84ba4e5
casterHoster <95349741+casterHoster@users.noreply.github.com>
2025-01-13 16:18:44 +0300

    set settings

---
+++ Assets/Scripts/Union.cs
@@ -0,0 +1,22 @@
+
+public delegate void ShowCell(float x, float y, float gift);
+
+public class Union
+{
+    private ShowCell _cell;
+
+    public Union(ShowCell cell)
+    {
+        _cell = cell;
+    }
+
+    public void Start()
+    {
+
+    }
+
+    public void Click(float x, float y)
+    {
+
+    }
+}
