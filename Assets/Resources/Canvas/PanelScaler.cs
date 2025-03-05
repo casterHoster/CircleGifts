beb0dfd1830dd6c1f1ee079e22eb53ff004a88fb
casterHoster <95349741+casterHoster@users.noreply.github.com>
2025-03-05 20:37:49 +0300

    is done

---
+++ Assets/Resources/Canvas/PanelScaler.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PanelScaler : MonoBehaviour
+{
+    private RectTransform _rectTransform;
+
+    private void Awake()
+    {
+        Scaler scaler = new Scaler();
+        _rectTransform = GetComponent<RectTransform>();
+        _rectTransform.localScale = new Vector3(_rectTransform.localScale.x * scaler.Scaling, _rectTransform.localScale.y * scaler.Scaling);
+    }
+}
